511f289ea6aaa35fbec1fac0af45404a4be73207
Dylan Jasper <dylanejasper@gmail.com>
2020-07-01 21:11:42 -0700

    updated scripts

--- ec2-scripts/ec2-development.sh
+++ ec2-scripts/ec2-development.sh
@@ -10,13 +10,10 @@ yum install -y git
 
 # Configure/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone https://github.com/dejasper1/python-image-gallery-1.git
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
-cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/java-image-gallery.git
-chown -R ec2-user:ec2-user java-image-gallery
 
 # Start/enable services
 systemctl stop postfix
